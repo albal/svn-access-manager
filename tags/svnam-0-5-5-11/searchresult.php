43d7a0c7cbdfeb814770b78dae737ce1575a178d
tom_krieger <tom_krieger@c8578496-874d-0410-b554-c924dcffaf98>
2012-10-13 20:01:53 +0000

    New release 0.5.5.11

---
+++ tags/svnam-0-5-5-11/searchresult.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+    SVN Access Manager - a subversion access rights management tool
+    Copyright (C) 2008 Thomas Krieger <tom@svn-access-manager.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+if ( file_exists ( realpath ( "./config/config.inc.php" ) ) ) {
+	require( "./config/config.inc.php" );
+} elseif( file_exists ( realpath ( "../config/config.inc.php" ) ) ) {
+	require( "../config/config.inc.php" );
+} elseif( file_exists( "/etc/svn-access-manager/config.inc.php" ) ) {
+	require( "/etc/svn-access-manager/config.inc.php" );
+} else {
+	die( "can't load config.inc.php. Check your installation!\n" );
+}
+
+$installBase					= isset( $CONF['install_base'] ) ? $CONF['install_base'] : "";
+
+require ("$installBase/include/variables.inc.php");
+require_once ("$installBase/include/functions.inc.php");
+require_once ("$installBase/include/db-functions-adodb.inc.php");
+include_once ("$installBase/include/output.inc.php");
+
+initialize_i18n();
+
+$SESSID_USERNAME 							= check_session ();
+check_password_expired();
+$dbh										= db_connect();
+$preferences								= db_get_preferences($SESSID_USERNAME, $dbh );
+$CONF['page_size']							= $preferences['page_size'];
+$rightAllowed								= db_check_acl( $SESSID_USERNAME, 'User admin', $dbh );
+$_SESSION['svn_sessid']['helptopic']		= "list_users";
+$error                                      = 0;
+$tErrorClass                                = "";
+$tMessage                                   = "";
+$tArray                                     = isset( $_SESSION['svn_sessid']['searchresult'] ) ? $_SESSION['svn_sessid']['searchresult'] : array();
+$tType                                      = isset( $_SESSION['svn_sessid']['searchtype'] )   ? $_SESSION['svn_sessid']['searchtype']   : "";
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    $template               	            = "searchresult_$tType.tpl";
+    $header									= "users";
+   	$subheader								= "users";
+   	$menu									= "users";
+
+    include ("./templates/framework.tpl");
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $template                	           	= "searchresult_$tType.tpl";
+    $header									= "users";
+   	$subheader								= "users";
+   	$menu									= "users";
+
+    include ("./templates/framework.tpl");
+}
+
+db_disconnect( $dbh );
+
+?>
