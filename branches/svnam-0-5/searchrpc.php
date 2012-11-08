1f88e4a86b937b081d9d64e09a91bac699a9a57d
tom_krieger <tom_krieger@c8578496-874d-0410-b554-c924dcffaf98>
2012-11-08 16:54:30 +0000

    filter for access rights

--- branches/svnam-0-5/searchrpc.php
+++ branches/svnam-0-5/searchrpc.php
@@ -45,6 +45,11 @@ $maxRows                                                                = isset(
 $filter                                                                 = isset( $_GET['name_startsWith'] )     ? ( $_GET['name_startsWith'] )  	: "";
 $db                                                                     = isset( $_GET['db'] )              	? ( $_GET['db'] )                   : "";
 $userid																	= isset( $_GET['userid'] )				? ( $_GET['userid'] )				: "";
+$user																	= isset( $_GET['user'] )				? ( $_GET['user'] )					: "";
+$group																	= isset( $_GET['group'] )				? ( $_GET['group'] )				: "";
+$project																= isset( $_GET['project'] )				? ( $_GET['project'] )				: "";
+$admin																	= isset( $_GET['admin'] )				? ( $_GET['admin'] )				: "";
+$allowed																= isset( $_GET['allowed'] )				? ( $_GET['allowed'] )				: "";
 $tArray                                                                 = array();
 
 list($ret, $SESSID_USERNAME)            								= check_session_status();
@@ -278,9 +283,245 @@ if( $ret == 0 ) {
 	}
 	
 	db_disconnect( $dbh );
+	
+} elseif( strtolower($db) == "accessrighttable" ) {
+	
+	error_log( "user = $user - group = $group - project = $project" );
+	$dbh																= db_connect();	
+	$schema																= db_determine_schema();
+	$tProjectIds														= "";
+	
+	if( $userid != -1 ) {
+		$id									= db_getIdByUserid( $userid, $dbh );
+		$tProjectIds						= "";
+		$query								= "SELECT * " .
+	  					      				  "  FROM ".$schema."svn_projects_responsible " .
+	  					      				  " WHERE (user_id = $id) " .
+	  					      				  "   AND (deleted = '00000000000000')";
+	} else {
+		
+		$tProjectIds						= "";
+		$query								= "SELECT * " .
+	  					      				  "  FROM ".$schema."svn_projects_responsible " .
+	  					      				  " WHERE (deleted = '00000000000000')";
+	  					      				  
+	}
+	
+  	$result									= db_query( $query, $dbh );
+  	while( $row = db_assoc( $result['result'] ) ) {
+  		
+  		if( $tProjectIds == "" ) {
+  			
+  			$tProjectIds 					= $row['project_id'];
+  			
+  		} else {
+  			
+  			$tProjectIds					= $tProjectIds.",".$row['project_id'];
+  			
+  		}
+  		
+  	}
+  	
+  	error_log("Project Ids: $tProjectIds");
+  	
+  	print "<thead>\n";
+	print "	<tr>\n";
+	print "	<th align='center' class='ui-table-default'>\n";
+	print "		<strong>"._('M')."</strong>\n";
+	print "	</th>\n";
+	print "	<th class='ui-table-default'>\n";
+	print "		<strong>"._('Project')."</strong>\n";
+	print "	</th>\n";
+	print "	<th class='ui-table-default'>\n";
+	print "		<strong>"._('Rights')."</strong>\n";
+	print "	</th>\n";
+	print "	<th align='center' class='ui-table-default'>\n";
+	print "		<strong>"._('User')." </strong>\n";
+	print "	</th>\n";
+	print "	<th align='center' class='ui-table-default'>\n";
+	print "		<strong>"._('Group')."</strong>\n";
+	print "	</th>\n";
+	print "	<th align='center' class='ui-table-default'>\n";
+	print "		<strong>"._('Valid from')."</strong>\n";
+	print "	</th>\n";
+	print "	<th align='center' class='ui-table-default'>\n";
+	print "		<strong>"._('Valid until')."</strong>\n";
+	print "	</th>\n";
+	print "	<th class='ui-table-default'>\n";
+	print "		<strong>"._('Repository:Directory')."</strong>\n";
+	print "	</th>\n";
+	print "	<th class='ui-table-deactivate'>\n";
+	print "		<strong>"._('Action')."</strong>\n";
+	print "	</th>\n";
+	print "</tr>\n";
+	print "<tr class='ui-table-deactivate'>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		<strong>"._('Filter:')."</strong>\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		<input id='filterproject' class='large' type='text' name='fSearchProject' value='' title='"._('Search access rights by project.')."' />\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		&nbsp;\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		<input id='filteruser' class='large' type='text' name='fSearchUser' value='' title='"._('Search access rights by user.')."' />\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		<input id='filtergroup' class='large' type='text' name='fSearchGroup' value='' title='"._('Search access rights by group.')."' />\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		<input type='image' name='fSubmit_clear' src='./images/button_cancel.png' value='"._("Clear filter")." title='"._("Clear filter")."' />\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		&nbsp;\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		&nbsp;\n";
+	print "	</td>\n";
+	print "	<td class='ui-table-deactivate'>\n";
+	print "		&nbsp;\n";
+	print "	</td>\n";
+	print "</tr>\n";
+	print "</thead>\n";
+	print "<tbody id='tbody'>\n";
+
+  	
+  	$curdate								= date( "Ymd" );
+  	$query									= "SELECT svn_access_rights.id AS rid, svnmodule, modulepath, svnrepos." .
+											  "       reponame, valid_from, valid_until, path, access_right, recursive," .
+											  "       svn_access_rights.user_id, svn_access_rights.group_id, repopath " .
+											  "  FROM ".$schema."svn_access_rights, ".$schema."svnprojects, ".$schema."svnrepos " .
+											  " WHERE (svnprojects.id = svn_access_rights.project_id) " .
+											  "   AND (svnprojects.id IN (".$tProjectIds."))" .
+											  "   AND (svnprojects.repo_id = svnrepos.id) " .
+											  "   AND (svn_access_rights.deleted = '00000000000000') ";
+	if( $project != "" ) {
+		$query								= $query."   AND ((svnprojects.svnmodule like '%$project%') ".
+												     "    OR  (svnprojects.description like '%$projects%')) ";
+	}											  
+	$query									= $query."ORDER BY svnrepos.reponame, svn_access_rights.path ";
+	error_log( $query );
+	$result									= db_query( $query, $dbh );
+	$i 										= 0;
+
+	while( $row = db_assoc( $result['result'] ) ) {
+	
+		$use								= true;
+		$name								= "";
+		if( ( $row['user_id'] != 0 )  ) {
+			list( $use, $name)				= checkData( 'user', $row['user_id'], $user, $dbh );
+		}
+		if( ( $row['group_id'] != 0 )  ) {
+			list( $use, $name )				= checkdata( 'group', $row['group_id'], $group, $dbh );
+		}
+		
+		if( $use ) { 
+			$row['username']				= $name;
+			$id								= $row['rid'];
+			$validfrom						= splitValidDate( $row['valid_from'] );
+			$validuntil						= splitValidDate( $row['valid_until'] );
+			$field							= "fDelete".$i;
+			$action							= "";
+			
+			if( $allowed == "edit" ) {
+				$url						= htmlentities("workOnAccessRight.php?id=".$row['rid']."&task=change");
+				$action						= "<a href=\"$url\" title=\""._("Change")."\" alt=\""._("Change")."\"><img src=\"./images/edit.png\" border=\"0\" /></a>";
+			} elseif( $allowed == "delete" ) {
+				$url						= htmlentities("workOnAccessRight.php?id=".$row['rid']."&task=change");
+				$action						= "<a href=\"$url\" title=\""._("Change")."\" alt=\""._("Change")."\"><img src=\"./images/edit.png\" border=\"0\" /></a>     <a href=\"deleteAccessRight.php?id=".htmlentities($row['rid'])."&task=delete\" title=\""._("Delete")."\" alt=\""._("Delete")."\"><img src=\"./images/edittrash.png\" border=\"0\" /></a>";
+			} elseif( $admin == "p" ) {
+				$url						= htmlentities("workOnAccessRight.php?id=".$row['rid']."&task=change");
+				$action						= "<a href=\"workOnAccessRight.php?id=".$row['rid']."&task=change\" title=\""._("Change")."\" alt=\""._("Change")."\"><img src=\"./images/edit.png\" border=\"0\" /></a>";
+				$action						= "<a href=\"$url\" title=\""._("Change")."\" alt=\""._("Change")."\"><img src=\"./images/edit.png\" border=\"0\" /></a>     <a href=\"deleteAccessRight.php?id=".htmlentities($row['rid'])."&task=delete\" title=\""._("Delete")."\" alt=\""._("Delete")."\"><img src=\"./images/edittrash.png\" border=\"0\" /></a>";
+			}
+			
+			print "\t\t\t\t\t<tr valign=\"top\">\n";
+			print "\t\t\t\t\t\t<td><input type=\"checkbox\" name=\"".$field."\" value=\"".$row['rid']."\"/></td>\n";
+			print "\t\t\t\t\t\t<td>".$row['svnmodule']."</td>\n";
+			print "\t\t\t\t\t\t<td align=\"center\">".$row['access_right']."</td>\n";
+			print "\t\t\t\t\t\t<td>".$row['username']."</td>\n";
+			print "\t\t\t\t\t\t<td> </td>\n";
+			print "\t\t\t\t\t\t<td>".$validfrom."</td>\n";
+			print "\t\t\t\t\t\t<td>".$validuntil."</td>\n";
+			print "\t\t\t\t\t\t<td>".$row['reponame'].":".$row['path']."</td>\n";
+			print "\t\t\t\t\t\t<td nowrap>".$action."</td>\n";
+			print "\t\t\t\t\t</tr>\n";
+			
+			$i++;
+		}			   		
+	}
+	
+	print "</tbody>";
+	
+	db_disconnect( $dbh );
 }
 
-$data                                                                   = json_encode($tArray);
+if( strtolower($db) != "accessrighttable" ) {
 
-print $callback."(".$data.");"
+	$data                                                                   = json_encode($tArray);
+	print $callback."(".$data.");";	
+}
+
+function checkData( $type, $id, $value, $dbh ) {
+	
+	global $CONF;
+	
+	$schema																= db_determine_schema();
+	if( $type == "group" ) {
+		
+		$query															= "SELECT groupname, description ".
+																		  "  FROM svngroups ".
+																		  " WHERE (id = $id)";
+																		  
+	} elseif( $type == "user" ) {
+		
+		$query															= "SELECT name, givenname, userid ".
+																		  "  FROM svnusers ".
+																		  " WHERE (id = $id)";
+																		  
+	} else {
+		
+		return( array(false, '') );
+		
+	}
+	
+	$result																= db_query( $query, $dbh );
+	if( $result['rows'] == 1 ) {
+		$row															= db_assoc( $result['result'] );
+		
+		if( $type == "group" ) {
+			
+			if( 
+				( preg_match( "/$value/", $row['groupname']) ) 	or
+				( preg_match( "/$value/", $row['description']) )
+			) {
+				return( array(true, '') );
+			} else {
+				
+				return( array(false, '') );
+			}
+			
+		} elseif( $type == "user" ) {
+			
+			if( 
+				( preg_match( "/$value/", $row['name'] ) ) 		or
+				( preg_match( "/$value/", $row['givenname'] ) )	or
+				( preg_match( "/$value/", $row['userid'] ) )
+			) {
+				if( $row['givenname'] != "" ) {
+					$name												= $row['givenname']." ".$row['name'];
+				} else {
+					$name												= $row['name'];
+				}
+				return( array(true, $name) );
+			} else {
+				return( array(false, '') );
+			}
+		}
+		
+	} else {
+		return( array(false, '') );
+	}
+}
 ?>
