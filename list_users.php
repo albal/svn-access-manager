d6098778afe89a3afef41ad278b78478ff8c0b05
tom_krieger <52508884+tom-krieger@users.noreply.github.com>
2012-11-18 08:25:56 +0000

    corrected escape_string

--- list_users.php
+++ list_users.php
@@ -153,7 +153,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		$button									= "undef";
 	}
 	
-	$tSearch                                    = isset( $_POST['fSearch'] )    ? escape_string( $_POST['fSearch'] )        : "";
+	$tSearch                                    = isset( $_POST['fSearch'] )    ? db_escape_string( $_POST['fSearch'] )        : "";
  	
  	if( ($button == "search") or ($tSearch != "") ) {
 
