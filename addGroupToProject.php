c38dd6c1eacf5749d55d0c1d3f94479aaf9c61d2
tom_krieger <52508884+tom-krieger@users.noreply.github.com>
2017-12-14 15:40:52 +0000

    new erroe message if config.inc.php is not found

--- addGroupToProject.php
+++ addGroupToProject.php
@@ -35,7 +35,7 @@ if (file_exists ( realpath ( "./config/config.inc.php" ) )) {
 } elseif (file_exists ( "/etc/svn-access-manager/config.inc.php" )) {
     require ("/etc/svn-access-manager/config.inc.php");
 } else {
-    die ( "can't load config.inc.php. Check your installation!\n" );
+    die ( "can't load config.inc.php. Please check your installation!\n" );
 }
 
 $installBase = isset ( $CONF ['install_base'] ) ? $CONF ['install_base'] : "";
